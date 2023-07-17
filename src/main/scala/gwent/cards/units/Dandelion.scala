2efa1c74ccbe06f7a2991766652bbbf04abc517d
ThePepe141 <jose.vasquez@ing.uchile.cl>
2023-07-17 13:53:43 -0400

    Add units and Controller class but they lack implementatio

---
+++ src/main/scala/gwent/cards/units/Dandelion.scala
@@ -0,0 +1,46 @@
+package cl.uchile.dcc
+package gwent.cards.units
+
+import gwent.cards.CloseCombatUnit
+
+import cl.uchile.dcc.gwent.cards.units.abilities.MoraleBoost
+
+abstract class Dandelion extends CloseCombatUnit("Dandelion", 2) with MoraleBoost{
+
+  //Equals y hashCode -------------------------------------------
+
+  /** A function that compares the types of two values.
+   *
+   * @param that The other value to compare.
+   * @return True if the two values have the same type.
+   */
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Dandelion]
+
+  /** A function that compares the structure of two objects.
+   *
+   * @param that The object to compare.
+   * @return True if the two objects have the same structure.
+   */
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      super.equals(that)
+    }
+    else {
+      false
+    }
+  }
+
+  /** A function that generates a hash value to an object.
+   *
+   * @return The hash value.
+   */
+  override def hashCode(): Int = {
+    val primo = 31
+    var total = 1
+    total = primo * total + classOf[Dandelion].##
+    total = primo * total + name.##
+    total = primo * total + basePower
+    total
+  }
+  //----------------------------------------------------------
+}
--- src/test/scala/CardTest.scala
+++ src/test/scala/CardTest.scala
@@ -1,7 +1,7 @@
 package cl.uchile.dcc
 package gwent.cards
 
-import cl.uchile.dcc.gwent.cards.units.{Geralt, RedanianArcher, ReinforcedTrebuchet, TemerianInfantry}
+import cl.uchile.dcc.gwent.cards.units.{BlueStripesCommando, Catapult, CrinfridReaversHunter, Dandelion, Geralt, KaedweniSiegeExpert, RedanianArcher, ReinforcedTrebuchet, TemerianInfantry}
 import cl.uchile.dcc.gwent.cards.weathers.SunnyDay
 import munit.FunSuite
 
@@ -18,6 +18,17 @@ class CardTest extends FunSuite {
   var archer2: RedanianArcher = _
   var engine1: ReinforcedTrebuchet = _
   var engine2: ReinforcedTrebuchet = _
+  /*
+  var commando1: BlueStripesCommando = _
+  var commando2: BlueStripesCommando = _
+  var catapult1: Catapult = _
+  var catapult2: Catapult = _
+  var hunter1: CrinfridReaversHunter = _
+  var hunter2: CrinfridReaversHunter = _
+  var bard1: Dandelion = _
+  var bard2: Dandelion = _
+  var expert1: KaedweniSiegeExpert = _
+  var expert2: KaedweniSiegeExpert = _*/
 
   override def beforeEach(context: BeforeEach): Unit = {
     Geralt = new Geralt()
